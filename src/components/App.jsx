c45144680b542a937d06b864a1fd044c42db6d4a
Mart <marta.nikiel@yahoo.com>
2023-12-13 09:08:23 +0100

    copied files from hw 06, installed all frameworks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2> Contacts</h2>
+      <div
+        style={{
+          boxShadow: '0px 0px 6px 5px rgba(128, 187, 236, 0.75)',
+          borderRadius: '4px',
+          padding: '20px',
+        }}
+      >
+        <Filter />
+        <ContactList />
+      </div>
     </div>
   );
 };
